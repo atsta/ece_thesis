c28e25506ce73a2e4a0c12cc01847674b9c9150f
Athina <ath.ctam@gmail.com>
2019-05-11 20:02:45 +0300

    connect django with postgres

--- interface/pro2/pro2/settings.py
+++ interface/pro2/pro2/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'pro2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "energy_db",
+        'USER' : "postgres",
+        'PASSWORD': "45452119",
+        'HOST' : 'localhost',
     }
 }
 
