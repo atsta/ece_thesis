91174799900286bc779533dcab1c1fb9c76fb8cb
Athina <ath.ctam@gmail.com>
2019-06-19 00:13:24 +0300

    models register admin site

--- MuPIA/pro2/app2/admin.py
+++ MuPIA/pro2/app2/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from app2.models import Measure, Social
+from app2.models import Measure, Social, Portfolio, Financial, Perspective
 
 # Register your models here.
 # in oreder to see them when logged in as admin 
 admin.site.register(Measure)
+admin.site.register(Portfolio)
+admin.site.register(Financial)
 admin.site.register(Social)
+admin.site.register(Perspective)
